fab88910235e73cdded88abfa345b32ba8034b21
SOFe <sofe2038@gmail.com>
2017-02-27 00:08:14 +0800

    Nah, I'll keep it Unlicense.

--- SimplePermissions/src/SOFe/SimplePermissions/SimplePermissions.php
+++ SimplePermissions/src/SOFe/SimplePermissions/SimplePermissions.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
- *
- * SimplePermissions
- *
- * Copyright (C) 2017 SOFe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
-*/
-
 namespace SOFe\SimplePermissions;
 
 use pocketmine\command\Command;
